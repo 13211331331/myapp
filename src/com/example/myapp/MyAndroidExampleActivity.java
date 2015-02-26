887b5e927a9f460fa4bb0814e9d810a62aea7ace
keney <1365977142@qq.com>
2015-02-26 16:14:54 +0800

    button

--- src/com/example/myapp/MyAndroidExampleActivity.java
+++ src/com/example/myapp/MyAndroidExampleActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import com.example.myapp.activities.AlarmManager.AlarmTestActivity;
 import com.example.myapp.activities.GestureActivity.GestureTest;
+import com.example.myapp.activities.button.ButtonTest;
 import com.example.myapp.activities.time.Timetest;
 
 
@@ -44,8 +45,8 @@ public class MyAndroidExampleActivity extends Activity implements
         b1.setOnClickListener(this);
         b2.setOnClickListener(this);
         b3.setOnClickListener(this);
-         /*
         b4.setOnClickListener(this);
+         /*
         b5.setOnClickListener(this);
         b6.setOnClickListener(this);
         b7.setOnClickListener(this);
@@ -70,8 +71,8 @@ public class MyAndroidExampleActivity extends Activity implements
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
-        /*
         b4 = (Button) findViewById(R.id.b4);
+        /*
         b5 = (Button) findViewById(R.id.b5);
         b6 = (Button) findViewById(R.id.b6);
         b7 = (Button) findViewById(R.id.b7);
@@ -107,12 +108,12 @@ public class MyAndroidExampleActivity extends Activity implements
                 intent.setClass(getApplicationContext(), Timetest.class);
                 startActivity(intent);
                 break;
-            /*
+
             case R.id.b4:
                 intent.setClass(getApplicationContext(), ButtonTest.class);
                 startActivity(intent);
-
                 break;
+            /*
             case R.id.b5:
                 intent.setClass(getApplicationContext(), ListTest.class);
                 startActivity(intent);
